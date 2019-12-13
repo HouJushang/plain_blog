04731e0cc8439f0d2a143598fdc6abaa12458789
5466315@qq.com <h1116761886>
2019-12-13 17:44:15 +0800

    123

--- src/com/houjushang/Main.java
+++ src/com/houjushang/Main.java
@@ -1,7 +1,9 @@
 package com.houjushang;
 
+import com.houjushang.framework.Application;
+
 public class Main {
     public static void main(String[] args) {
-
+        Application.start();
     }
 }
--- src/com/houjushang/framework/Application.java
+++ src/com/houjushang/framework/Application.java
@@ -1,4 +1,7 @@
 package com.houjushang.framework;
 
 public class Application {
+    public static void start() {
+        System.out.println(123);
+    }
 }
