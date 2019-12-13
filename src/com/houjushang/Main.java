67e577c58c61c71f73f4d3737e0e7faa9bec7d6d
5466315@qq.com <h1116761886>
2019-12-13 15:50:02 +0800

    init project

---
+++ src/com/houjushang/Main.java
@@ -0,0 +1,7 @@
+package com.houjushang;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/houjushang/framework/Application.java
@@ -0,0 +1,4 @@
+package com.houjushang.framework;
+
+public class Application {
+}
